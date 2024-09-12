a5393b39d7c58d056f3e3cae561b1015341e7bb1
Filip Rehacek <filiprehacek4@gmail.com>
2024-09-12 13:00:37 +0200

    created and finished project pruchod_po_indexech

---
+++ 00_opakovani-pruchod_pole_po_indexech/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _00_opakovani_pruchod_pole_po_indexech
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int[] array = { -5, 3, 2, -3, 7, 0, 12, -1 };
+            //int[] array = {1, 3, 2, 1, 7, 0, 12, -1};
+            //int[] array = {-1};
+            int[] array = {};
+
+            int sumS = 0,  sumL = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i%2 == 0)
+                {
+                    sumS += array[i];
+                } else
+                {
+                    sumL += array[i];
+                }
+            }
+            Console.WriteLine(sumS);
+            Console.WriteLine(sumL);
+        }
+    }
+}
