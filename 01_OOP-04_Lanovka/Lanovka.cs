10ab3142514138302b226df2fde54ba2ef7d5284
Filip Rehacek <filiprehacek4@gmail.com>
2024-09-24 10:25:09 +0200

    Solved Lanovka

---
+++ 01_OOP-04_Lanovka/Lanovka.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_OOP_04_Lanovka
+{
+    internal class Lanovka
+    {
+        private int delka;
+        private double nosnost;
+        public double Zatizeni;
+        public bool JeVolnoDole;
+        public bool JeVolnoNahore;
+        private Clovek[] lanovka;
+
+        public Lanovka(int Delka, double Nostnost)
+        {
+            JeVolnoDole = true;
+            JeVolnoNahore = true;
+            nosnost = Nostnost;
+            Zatizeni = 0;
+            lanovka = new Clovek[Delka];
+        }
+
+        public bool Nastup(Clovek clovek)
+        {
+            if (lanovka[0] != null || nosnost-Zatizeni-clovek.Hmotnost < 0) return false;
+            lanovka[0] = clovek;
+            Zatizeni += clovek.Hmotnost;
+            return true;
+        }
+
+        public Clovek Vystup()
+        {
+            Clovek vystup = lanovka[lanovka.Length - 1];
+            lanovka[lanovka.Length - 1] = null;
+            return vystup;
+        }
+
+        public void Jed()
+        {
+            if (lanovka[lanovka.Length - 1] != null) throw new ArgumentOutOfRangeException();
+            for (int i = lanovka.Length - 1; i > 0; i--)
+            {
+                lanovka[i] = lanovka[i - 1];
+            }
+            lanovka[0] = null;
+        }
+
+
+
+    }
+
+}
