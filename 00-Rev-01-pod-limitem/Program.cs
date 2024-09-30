fba75431b5773bb2e0016c1bb8af93f3a7f86300
Filip Rehacek <filiprehacek4@gmail.com>
2024-09-30 12:43:00 +0200

    added 2 projects

---
+++ 00-Rev-01-pod-limitem/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _00_Rev_01_pod_limitem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] cisla = { 1.3, 2.2, -1.5, 1.4, -2.7 };
+            Console.WriteLine(PrumerPodLimitem(cisla, 1.1));
+            Console.WriteLine(PrumerPodLimitem(cisla, -2));
+        }
+
+        static double PrumerPodLimitem(double[] cisla, double limit)
+        {
+            double soucet = 0;
+            int pocet = 0;
+            foreach (double cislo in cisla)
+            {
+                if (cislo < limit)
+                {
+                    soucet += cislo;
+                    pocet++;
+                }
+            }
+            if (soucet/pocet == double.NaN) return 0;
+            else return soucet/pocet;
+            
+        }
+    }
+}
---
+++ 00-Rev-01-pred-maximem/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace _00_Rev_01_pred_maximem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] cisla = vstup();
+            Console.WriteLine(GetCisloPredNejvetsim(cisla));
+        }
+
+        static int[] vstup()
+        {
+            string input;
+            int[] cisla = { };
+            int i = -1;
+            try
+            {
+                do
+                {
+                    input = Console.ReadLine();
+                    i++;
+                    cisla.Append(1);
+                } while (input != "dost" || int.TryParse(input, out cisla[i]));
+                return cisla;
+
+
+            }
+            catch (System.IndexOutOfRangeException)
+            {
+                return cisla;
+            }
+            
+        }
+
+        static string GetCisloPredNejvetsim(int[] cisla)
+        {
+            if (cisla.Length == 0) return "Nebylo zadáno žádné číslo";
+            int i_nejvetsi = 0;
+            int cislo_nejvetsi = cisla[0];
+
+            for (int i = 1; i < cisla.Length; i++)
+            {
+                if (cisla[i] > cislo_nejvetsi)
+                {
+                    i_nejvetsi = i;
+                    cislo_nejvetsi = cisla[i];
+                }
+            }
+            if (i_nejvetsi == 0) return cisla[cisla.Length - 1].ToString();
+            else return cisla[i_nejvetsi-1].ToString();
+        }
+    }
+}
